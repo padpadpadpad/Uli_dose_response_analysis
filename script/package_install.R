96eb44cf2c15cda68514aae7897dc67c1f19d6a5
Daniel Padfield <d.padfieldscfc@gmail.com>
2017-07-28 15:16:37 +0100

    streamline package_install

--- script/package_install.R
+++ script/package_install.R
@@ -7,5 +7,8 @@ pkgs <- c('tidyr',
           'nlme',
           'readxl')
 
+# check if they are already installed
+new_pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+
 # install packages
-install.packages(pkgs)
+if(length(new_pkgs)) install.packages(new_pkgs)
